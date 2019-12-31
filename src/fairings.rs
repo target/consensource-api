ecca90932f55e8c1078ee18dec64aefc5f036efe
Patrick-Erichsen <Patrick.Erichsen@target.com>
2019-12-31 12:28:57 -0600

    Add tests for CORS fairings
    Signed-off-by: Patrick-Erichsen <Patrick.Erichsen@target.com>

--- src/fairings.rs
+++ src/fairings.rs
@@ -34,3 +34,69 @@ impl Fairing for CORS {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rocket::local::Client;
+    use rocket_contrib::json::JsonValue;
+
+    #[options("/")]
+    fn options_route() -> &'static str {
+        "Hello, world!"
+    }
+
+    #[get("/")]
+    fn get_json_route() -> JsonValue {
+        json!("Hello, world!")
+    }
+
+    fn get_rocket_client() -> Client {
+        let rocket = rocket::ignite()
+            .mount("/", routes![options_route, get_json_route])
+            .attach(CORS());
+        let client = Client::new(rocket).expect("valid rocket instance");
+        client
+    }
+
+    #[test]
+    fn test_method_options() {
+        let client = get_rocket_client();
+        let req = client.options("/");
+        let mut response = req.dispatch();
+        let headers = response.headers();
+
+        assert_eq!(headers.get_one("Access-Control-Allow-Origin"), Some("*"));
+        assert_eq!(
+            headers.get_one("Access-Control-Allow-Methods"),
+            Some("POST, GET, OPTIONS, PATCH")
+        );
+        assert_eq!(
+            headers.get_one("Access-Control-Allow-Credentials"),
+            Some("true")
+        );
+        assert_eq!(
+            headers.get_one("Content-Type"),
+            Some("text/plain; charset=utf-8")
+        );
+        assert_eq!(response.body_string(), Some("".to_string()));
+    }
+
+    #[test]
+    fn test_method_json() {
+        let client = get_rocket_client();
+        let req = client.get("/");
+        let response = req.dispatch();
+        let headers = response.headers();
+
+        assert_eq!(headers.get_one("Access-Control-Allow-Origin"), Some("*"));
+        assert_eq!(
+            headers.get_one("Access-Control-Allow-Methods"),
+            Some("POST, GET, OPTIONS, PATCH")
+        );
+        assert_eq!(
+            headers.get_one("Access-Control-Allow-Credentials"),
+            Some("true")
+        );
+    }
+}
